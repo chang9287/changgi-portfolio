75e49e92afd052303118dec88129ecadf297d9ec
chang9287 <ckdrl9287@gmail.com>
2026-04-21 23:03:03 +0900

    icon.ts, skills.ts 데이터 연결 및 Skill 섹션 초안 작성

--- portfolio-react/src/sections/about/SkillSection.tsx
+++ portfolio-react/src/sections/about/SkillSection.tsx
@@ -4,16 +4,34 @@ import { skills } from "../../data/skills";
 
 
 export default function SkillSection() {
-  /* category 선택 */
-  const [activeCategory, setActiveCategory] = useState("");
+  
+  /* Categories 메뉴 */
+  const categories = [
+    "Language", "", ""
+  ]
 
-  /* icon 선택 */
-  const [selectedIcon, setSelectedIcon] = useState();
+
+  /* 작업 순서 */
+  // Categories 배열을 가지고 메뉴 만들기
+  // 메뉴의 카테고리 클릭시 active상태
+  // 처음 상태는 아무것도 선택 안 된 상태
+  // 밑에 아이콘 이미지는 map으로 전부 다 불러오기 
+  // 메뉴가 클릭되어서 active상태라면 skill.category와 비교
+  // 같으면 가만히 두고 다르면 blur처리
+  // blur 된 아이콘은 클릭 불가능
+  // 아이콘 클릭시 선택된 아이콘에 해당되는 skill의 id값 저장
+  // 만약 다시 같은 아이콘 클릭하면 초기화
+  // 선택된 아이콘을 기준으로 skills에서 해당 아이콘 찾기
+  // 찾은 아이콘의 description을 출력하고
+  // 찾은 상태가 아니라면 "기술 아이콘을 선택하면 사용 경험을 볼 수 있습니다." 이게 출력 되어야함
 
   return (
     <section>
       <SectionTitle title={"skills"} />
       <div>
+        {skills.map((skill) => (
+          <div key={skill.id}>{skill.category}</div>
+        ))}
         {/* icon-category */}
         {/* 
           카테고리 선택 
