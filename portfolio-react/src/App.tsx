7efda1582fc2950a32366b42ca711e0695c5d7d4
chang9287 <ckdrl9287@gmail.com>
2026-05-05 00:11:15 +0900

    로딩페이지 완성

--- portfolio-react/src/App.tsx
+++ portfolio-react/src/App.tsx
@@ -1,10 +1,23 @@
 import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
 import Header from './components/common/Header'
 import Footer from './components/common/Footer'
 import { Home, About, Projects, Contact } from './pages'
+import Preloader from './components/ui/Preloader'
 import './App.css'
 
+
 function App() {
+  const [isLoading, setIsLoading] = useState(() => {
+    return sessionStorage.getItem("visited") ? false : true
+  });
+
+  if(isLoading) {
+    return <Preloader finishLoading = {() => {
+      sessionStorage.setItem("visited", "true");
+      setIsLoading(false);
+    }}/>
+  };
 
   return (
     <>
