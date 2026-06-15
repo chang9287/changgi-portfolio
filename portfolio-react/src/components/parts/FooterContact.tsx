f3e1dd0fbf4ab7261d6779012c0660a892ee87ed
chang9287 <ckdrl9287@gmail.com>
2026-06-15 23:16:16 +0900

    style: footer 반응형 레이아웃 구현

--- portfolio-react/src/components/parts/FooterContact.tsx
+++ portfolio-react/src/components/parts/FooterContact.tsx
@@ -24,21 +24,21 @@ export default function FooterContact({ className }: FooterContactProps ) {
     {
       icon: "xi-mail",
       href: "mailto:ckdrl9287@gmail.com",
-      liClass: "group hover:bg-icon-mail-hover-bg hover:border-icon-mail-hover-bg",
+      liClass: "bg-white group hover:bg-icon-mail-hover-bg hover:border-icon-mail-hover-bg",
       aClass: "w-full h-full flex items-center justify-center",
       iClass: "text-2xl group-hover:text-white transition duration-200",
     },
     {
       icon: "xi-call",
       href: "tel:01088809287",
-      liClass: "group hover:bg-icon-call-hover-bg hover:border-icon-call-hover-bg",
+      liClass: "bg-white group hover:bg-icon-call-hover-bg hover:border-icon-call-hover-bg",
       aClass: "w-full h-full flex items-center justify-center",
       iClass: "text-2xl group-hover:text-white transition duration-200",
     },
     {
       icon: "xi-github",
       href: "https://github.com/chang9287",
-      liClass: "group hover:bg-icon-github-hover-bg hover:border-icon-github-hover-bg",
+      liClass: "bg-white group hover:bg-icon-github-hover-bg hover:border-icon-github-hover-bg",
       aClass: "w-full h-full flex items-center justify-center",
       iClass: "text-2xl group-hover:text-icon-github-hover transition duration-200",
       isExternal: true
@@ -46,7 +46,7 @@ export default function FooterContact({ className }: FooterContactProps ) {
     {
       icon: "xi-kakaotalk",
       onClick: () => setIsOpen(true),
-      liClass: "group hover:bg-icon-kakao-hover-bg hover:border-icon-kakao-hover-bg",
+      liClass: "bg-white group hover:bg-icon-kakao-hover-bg hover:border-icon-kakao-hover-bg",
       aClass: "w-full h-full flex items-center justify-center",
       iClass: "text-2xl group-hover:text-icon-kakao-hover transition duration-200",
     }
@@ -55,7 +55,7 @@ export default function FooterContact({ className }: FooterContactProps ) {
   return (
     <>
       {/* FooterContact */}
-      <ul className={`flex gap-3 justify-center items-center mr-5 ${className}`}>
+      <ul className={`flex gap-2 lg:gap-3 justify-center items-center mr-4 lg:mr-5 ${className}`}>
         {iconStyles.map((icon, index) => (
           <li
             key={index}
