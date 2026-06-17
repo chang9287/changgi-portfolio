2703aed774a505d7fd00ebb18caf33f1698cf8e2
chang9287 <ckdrl9287@gmail.com>
2026-06-18 00:02:18 +0900

    feat: about 페이지 반응형 레이아웃 구현

--- portfolio-react/src/components/ui/EducationCard.tsx
+++ portfolio-react/src/components/ui/EducationCard.tsx
@@ -7,19 +7,19 @@ type EducationCardProps = {
 
 export default function EducationCard({ eduTitle, eduDetail, eduPeriod, eduResult }: EducationCardProps) {
   return (
-    <div className="mx-4 mt-8 flex flex-col gap-3">
-      <h3 className="text-3xl font-bold text-edu-title pl-4">
+    <div className="mx-4 mt-6 sm:mt-8 flex flex-col gap-3">
+      <h3 className="text-2xl lg:text-3xl font-bold text-edu-title px-2 sm:px-6">
         {eduTitle}
       </h3>
-      <div className="flex items-center justify-between px-6">
-        <span className="text-2xl font-bold">
+      <div className="flex flex-col gap-2 lg:gap-0 lg:flex-row lg:items-center justify-between px-2 sm:px-6">
+        <span className="text-lg sm:text-xl lg:text-2xl font-bold tracking-tight leading-[1.1]">
           {eduDetail}
         </span>
-        <span className="text-xl font-bold text-edu-period">
+        <span className="text-base sm:text-lg lg:text-xl self-end lg:self-auto font-bold text-edu-period">
           {eduPeriod}
         </span>
       </div>
-      <p className="text-lg font-bold text-edu-result px-6">
+      <p className="text-base lg:text-lg font-bold text-edu-result px-2 sm:px-6">
         {eduResult}
       </p>
     </div>
