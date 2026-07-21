e558471344b03e5b83a437caa2384252afe4fc80
안창기 <ckdrl9287@gmail.com>
2026-07-22 00:55:29 +0900

    feat: 장금이 프로젝트 추가

---
+++ portfolio-react/src/content/projects/janggeumi/images/index.ts
@@ -0,0 +1,34 @@
+import thumbnail from "./thumbnail.png";
+import mainPage from "./main-page.png";
+import searchPage from "./search-page.png";
+import loginPage from "./login-page.png";
+import productDetailPurchasePage from "./product-detail-purchase-page.png";
+import productDetailStoragePage from "./product-detail-storage-page.png";
+import productDetailProcessingPage from "./product-detail-processing-page.png";
+import productDetailStorageDetailPage from "./product-detail-storage-detail-page.png";
+import productDetailProcessingDetailPage from "./product-detail-processing-detail-page.png";
+
+
+type JanggeumiImages = {
+  thumbnail: string;
+  mainPage: string;
+  searchPage: string;
+  loginPage: string;
+  productDetailPurchasePage: string;
+  productDetailStoragePage: string;
+  productDetailProcessingPage: string;
+  productDetailStorageDetailPage: string;
+  productDetailProcessingDetailPage: string;
+}
+
+export const janggeumiImages: JanggeumiImages = {
+  thumbnail,
+  mainPage,
+  searchPage,
+  loginPage,
+  productDetailPurchasePage,
+  productDetailStoragePage,
+  productDetailProcessingPage,
+  productDetailStorageDetailPage,
+  productDetailProcessingDetailPage
+};
